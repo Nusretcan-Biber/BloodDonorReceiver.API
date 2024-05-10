423f17b54398290c89f88da8d1d324f64d78f6e7
Nusretcan-Biber <nusretcanbiber1@gmail.com>
2024-05-11 00:47:07 +0300

    Soma tables has added

---
+++ BloodDonorReceiver.Data/Enums/BloodTypeEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodDonorReceiver.Data.Enums
+{
+    public enum BloodTypeEnum
+    {
+      A_POSITIVE,
+      A_NEGATIVE,
+      B_POSITIVE,
+      B_NEGATIVE,
+      ZERO_POSITIVE,
+      ZERO_NEGATIVE,
+      AB_POSITIVE,
+      AB_NEGATIVE
+
+    }
+}
---
+++ BloodDonorReceiver.Data/Models/DonorModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodDonorReceiver.Data.Models
+{
+    public class DonorModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Birthday { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public  MyProperty { get; set; }
+
+    }
+}
