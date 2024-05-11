78d60f259b00963528688e1423c95b56b0cfa102
Nusretcan-Biber <nusretcanbiber1@gmail.com>
2024-05-11 17:04:41 +0300

    [master] Repository and UnitOfWork has added

---
+++ BloodDonorReceiver.Utils/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using BloodDonorReceiver.Utils.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodDonorReceiver.Utils.UnitOfWorks
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> GetRepository<T>() where T : class;
+        int SaveChanges();
+    }
+}
