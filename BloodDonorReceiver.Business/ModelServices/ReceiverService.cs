085e0e96d3d2a538d3abbcc4c783a2a3132dcb5a
Nusretcan-Biber <nusretcanbiber1@gmail.com>
2024-05-14 01:01:47 +0300

    [master] Something

---
+++ BloodDonorReceiver.Business/ModelServices/ReceiverService.cs
@@ -0,0 +1,13 @@
+﻿using BloodDonorReceiver.Business.IModelServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodDonorReceiver.Business.ModelServices
+{
+    public class ReceiverService : IReceiverService
+    {
+    }
+}
