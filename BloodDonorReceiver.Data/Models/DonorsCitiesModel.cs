0b4394ea4f39af872a192d49668aebdf1a3e5173
ayseakbaba <ayse.akbaba@detaysoft.com>
2024-05-20 01:20:16 +0300

    [master]: add donorscities and receiverscities models

---
+++ BloodDonorReceiver.Data/Models/DonorsCitiesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BloodDonorReceiver.Data.Models
+{
+    public class DonorsCitiesModel 
+    {
+        public Guid Guid { get; set; }
+        public Guid DonorsGuid { get; set; }
+        public int CitysId { get; set; }
+    }
+}
---
+++ BloodDonorReceiver.Data/Models/ReceiversCitiesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BloodDonorReceiver.Data.Models
+{
+    public class ReceiversCitiesModel
+    {
+        public Guid Guid { get; set; }
+        public Guid ReceiversGuid { get; set; }
+        public int CitysId { get; set; }
+    }
+}
